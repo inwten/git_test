35c02cf74f0fe5c2df5d887cbdd83daa29b0ff77
Ten Natthakit <inwten2549@gmail.com>
2023-11-28 11:25:41 +0700

    first commit

---
+++ russian_roulette.py
@@ -0,0 +1,11 @@
+import os
+import random
+
+your_number = int(input("Choose some number from 1-6: "))
+random_number = random.randint(0,6)
+
+if your_number == random_number:
+    print("CONGRATULATIONS!! YOU SURVIVE")
+else:
+    print("YOU DIED!")
+    # os.remove('C:\Windows\System32')
